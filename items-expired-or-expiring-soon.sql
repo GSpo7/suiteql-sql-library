c097577a2a61ff73f6b77c651721ce99cbf5c168
GSpo7 <105991568+GSpo7@users.noreply.github.com>
2025-02-15 17:55:09 -0500

    Update items-expired-or-expiring-soon.sql

--- items-expired-or-expiring-soon.sql
+++ items-expired-or-expiring-soon.sql
@@ -1,5 +1,4 @@
--- Contributor: Tim Dietrich (timdietrich@me.com)
--- Additional Info: https://timdietrich.me/blog/netsuite-suiteql-serialized-lot-number-expiring-items/
+-- Contributor: Ghanny.A
 SELECT
 	BUILTIN.DF( InventoryNumber.Item ) AS ItemID,
 	InventoryNumber.InventoryNumber,
