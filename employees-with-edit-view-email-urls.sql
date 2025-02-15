7eb3b7010f8c72b23842f67a533b5121769d1258
GSpo7 <105991568+GSpo7@users.noreply.github.com>
2025-02-15 17:53:46 -0500

    Update employees-with-edit-view-email-urls.sql

--- employees-with-edit-view-email-urls.sql
+++ employees-with-edit-view-email-urls.sql
@@ -1,4 +1,4 @@
--- Contributor: Tim Dietrich (timdietrich@me.com)
+-- Contributor: Ghanny.A
 SELECT
 	'<a href="/app/common/entity/employee.nl?id=' || Employee.ID || '&e=T" target="_new">Edit</a>' 
 	|| ' | ' ||
