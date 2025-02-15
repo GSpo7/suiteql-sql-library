3a7a1dbf19faac0d815dd194271476682575cba2
GSpo7 <105991568+GSpo7@users.noreply.github.com>
2025-02-15 17:54:52 -0500

    Update item-group-members.sql

--- item-group-members.sql
+++ item-group-members.sql
@@ -1,5 +1,4 @@
--- Contributor: Tim Dietrich (timdietrich@me.com)
--- Source: https://timdietrich.me/blog/netsuite-suiteql-item-groups/
+-- Contributor: Ghanny.A
 SELECT
 	ItemMember.Item,
 	MemberItem.ItemID,
