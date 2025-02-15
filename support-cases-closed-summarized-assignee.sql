7675a38278e617d07a6e60dd64b67130c0b0ddb7
GSpo7 <105991568+GSpo7@users.noreply.github.com>
2025-02-15 17:58:29 -0500

    Update support-cases-closed-summarized-assignee.sql

--- support-cases-closed-summarized-assignee.sql
+++ support-cases-closed-summarized-assignee.sql
@@ -1,5 +1,4 @@
--- Contributor: Tim Dietrich (timdietrich@me.com)
--- Additional Info: https://timdietrich.me/blog/netsuite-suiteql-support-cases/
+-- Contributor: Ghanny.A
 SELECT
 	BUILTIN.DF( Assigned ) AS AssignedTo,
 	COUNT(*) AS ClosedCaseCount,
